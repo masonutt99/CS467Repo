602b989889fb393c370ef89dd945648f70b675a3
masonutt99 <masonutt99@gmail.com>
2020-09-07 18:01:09 -0500

    build

---
+++ Client/UDP_Client.c
@@ -0,0 +1,50 @@
+//Code obtained and modified from https://www.geeksforgeeks.org/udp-server-client-implementation-c/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+#define PORT     4240
+#define MAXLINE 1024
+
+// Driver code
+int main() {
+    int sockfd; //Socket descriptor, like a file-handle
+    char buffer[MAXLINE]; //buffer to store message from server
+    char *hello = "Hello from client"; //message to send to server
+    struct sockaddr_in     servaddr;  //we don't bind to a socket to send UDP traffic, so we only need to configure server address
+
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Filling server information
+    servaddr.sin_family = AF_INET; //IPv4
+    servaddr.sin_port = htons(PORT); // port, converted to network byte order (prevents little/big endian confusion between hosts)
+    servaddr.sin_addr.s_addr = INADDR_ANY; //localhost
+
+    int n, len = sizeof(servaddr);
+    //Sending message to server
+    sendto(sockfd, (const char *)hello, strlen(hello), 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+    printf("Hello message sent.\n");
+
+    // Receive message from client
+    if(( n = recvfrom(sockfd, (char *)buffer, MAXLINE, 0, (struct sockaddr *) &servaddr, &len))<0)
+    {
+        perror("ERROR");
+        printf("Errno: %d. ",errno);
+        exit(EXIT_FAILURE);
+    }
+    buffer[n] = '\0'; //terminate message
+    printf("Server : %s\n", buffer);
+
+    close(sockfd);
+    return 0;
+}
